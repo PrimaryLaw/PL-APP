84319d55c4ce845170c12b548af26a75104b3047
Daniel Soares <daniel.soares@fortics.com.br>
2023-10-26 17:43:28 -0300

    fix: add manual build.

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -61,7 +61,7 @@ export async function GET() {
     });
     return NextResponse.json({ url: stripeSession.url });
   } catch (error) {
-    console.log("stripe error", error);
+    console.log("stripe error, route.ts", error);
     return new NextResponse("internal server error", { status: 500 });
   }
 }
