afa1cb771d9b998d4db4c1f6b05b3388ead71c8c
tais <tais.goncalves@qenta.com>
2023-10-31 20:14:45 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-3 text-5xl font-semibold">PrimaryLaw.ai</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
@@ -45,8 +45,7 @@ export default async function Home() {
           </div>
 
           <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join millions of students, researchers and professionals to instantly
-            answer questions and understand research with AI
+         TEST
           </p>
 
           <div className="w-full mt-4">
