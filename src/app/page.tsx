1771a916abde415706ed2f0e8715bf008961ae81
tais <tais.goncalves@qenta.com>
2023-11-01 18:41:52 +0000

    Customized the first page to launching soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,26 @@ import { chats } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 
 export default async function Home() {
-  const { userId } = await auth();
-  const isAuth = !!userId;
-  const isPro = await checkSubscription();
-  let firstChat;
-  if (userId) {
-    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
-    if (firstChat) {
-      firstChat = firstChat[0];
-    }
-  }
+ // const { userId } = await auth();
+ // const isAuth = !!userId;
+ // const isPro = await checkSubscription();
+ // let firstChat;
+ // if (userId) {
+ //   firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+ //   if (firstChat) {
+  //    firstChat = firstChat[0];
+  //  }
+  //}
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-zinc-900 to-zinc-900">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">PrimaryLaw.ai</h1>
-            <UserButton afterSignOutUrl="/" />
+      <h1 className="mr-3 text-7xl font-bold bg-gradient-to-r from-teal-600 to-slate-50 bg-clip-text text-transparent">We're lauching soon!</h1> 
+          {/*   <UserButton afterSignOutUrl="/" />  */}
           </div>
 
-          <div className="flex mt-2">
+         {/*    <div className="flex mt-2 ">
             {isAuth && firstChat && (
               <>
                 <Link href={`/chat/${firstChat.id}`}>
@@ -42,13 +42,13 @@ export default async function Home() {
                 </div>
               </>
             )}
-          </div>
+          </div> */ }
 
-          <p className="max-w-xl mt-1 text-lg text-slate-600">
-         TEST
+          <p className="max-w-xl mt-3 text-4xl font-extralight text-white">
+       PRIMARYLAW.AI
           </p>
 
-          <div className="w-full mt-4">
+          {/* <div className="w-full mt-4 ">
             {isAuth ? (
               <FileUpload />
             ) : (
@@ -59,7 +59,7 @@ export default async function Home() {
                 </Button>
               </Link>
             )}
-          </div>
+          </div> */ }
         </div>
       </div>
     </div>
