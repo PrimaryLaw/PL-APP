8e3432ac8c9fcee06ca2df5affa857fd60bdb360
tais <tais.goncalves@qenta.com>
2023-11-02 23:46:42 +0000

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,10 @@ export default async function Home() {
     }
   }
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100 ">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
-          <div className="flex items-center">
+          <div className="flex items-center hidden">
             <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
@@ -64,5 +64,7 @@ export default async function Home() {
         </div>
       </div>
     </div>
+
+   
   );
 }
