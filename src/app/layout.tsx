8c78febaecbce857001339db2fa3b65525be497b
tais <tais.goncalves@qenta.com>
2023-11-28 17:58:06 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,18 +26,6 @@ export default function RootLayout({
     }}>
       <Providers>
         <html lang="en">
-        <Head>
-          {/* Your Google Tag */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-0LN5S5YRCG"></script>
-          <script dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0LN5S5YRCG');
-            `,
-          }} />
-        </Head>
           <body className={inter.className}>{children}</body>
           <Toaster />
         </html>
