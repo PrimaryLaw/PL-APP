66a1c0d3c6d76ff2e7e406a4d3a98fc452f26c3e
tais <tais.goncalves@qenta.com>
2023-11-29 14:55:52 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import Providers from "@/components/Providers";
 import { Toaster } from "react-hot-toast";
 import Head from 'next/head';
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +24,22 @@ export default function RootLayout({
       },
     }}>
       <Providers>
+        <Head>
+          {/* Existing metadata and other head elements */}
+          {/* Google Tag Manager */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-0LN5S5YRCG"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-0LN5S5YRCG');
+              `,
+            }}
+          />
+        </Head>
         <html lang="en">
           <body className={inter.className}>{children}</body>
           <Toaster />
